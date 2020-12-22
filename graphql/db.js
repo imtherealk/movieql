17ccb5d3b081273b8fdecd3586b884e1fa4c8454
Nayeong <imtherealk@gmail.com>
2020-12-22 16:32:01 +0900

    Get movies

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,19 @@
-let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1,
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8,
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 99,
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2,
-  },
-];
+import fetch from "node-fetch";
 
-export const getMovies = () => movies;
+const BASE_URL = "https://yts-proxy.now.sh";
+const LIST_MOVIES_URL = `${BASE_URL}/list_movies.json?`;
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = LIST_MOVIES_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
-};
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: movies.length,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => {
+      return json.data.movies;
+    });
 };
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,24 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_, { id }) => getById(id),
-    movieList: (_, { ids }) => {
-      let filtered = [];
-      ids.forEach((id) => {
-        const movie = getById(id);
-
-        if (movie !== undefined) {
-          filtered.push(movie);
-        }
-      });
-      return filtered;
-    },
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
   },
 };
 
