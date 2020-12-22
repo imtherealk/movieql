42d3ff60aa07734f8b5474a1e3a9032eb0319635
Nayeong <imtherealk@gmail.com>
2020-12-22 16:49:36 +0900

    node-fetch -> axios

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,18 @@
-import fetch from "node-fetch";
+import axios from "axios";
 
 const BASE_URL = "https://yts-proxy.now.sh";
-const LIST_MOVIES_URL = `${BASE_URL}/list_movies.json?`;
+const LIST_MOVIES_URL = `${BASE_URL}/list_movies.json`;
 
-export const getMovies = (limit, rating) => {
-  let REQUEST_URL = LIST_MOVIES_URL;
-  if (limit > 0) {
-    REQUEST_URL += `limit=${limit}`;
-  }
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`;
-  }
-  return fetch(REQUEST_URL)
-    .then((res) => res.json())
-    .then((json) => {
-      return json.data.movies;
-    });
+export const getMovies = async (limit, rating) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating,
+    },
+  });
+  return movies;
 };
