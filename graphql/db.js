27d079752d2e2a9713ee85f16b177ca5f0323165
Nayeong <imtherealk@gmail.com>
2020-12-22 17:07:23 +0900

    Implement get movie detail, get suggestions

--- graphql/db.js
+++ graphql/db.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BASE_URL = "https://yts-proxy.now.sh";
 const LIST_MOVIES_URL = `${BASE_URL}/list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}/movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}/movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
   const {
@@ -12,6 +14,32 @@ export const getMovies = async (limit, rating) => {
     params: {
       limit,
       minimum_rating: rating,
+    },
+  });
+  return movies;
+};
+
+export const getMovie = async (id) => {
+  const {
+    data: {
+      data: { movie },
+    },
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id,
+    },
+  });
+  return movie;
+};
+
+export const getSuggestions = async (id) => {
+  const {
+    data: {
+      data: { movies },
+    },
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id,
     },
   });
   return movies;
